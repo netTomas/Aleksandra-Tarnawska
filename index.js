60e1f721e2a8a96135a8e13d6259f947c3d5aec4
Tomasz Tarnawski <tomasztarnawski73@gmail.com>
2023-01-29 18:08:38 +0100

    changed start in package

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.use('/handmade/product', desktop_product_handmadeRouter);
 app.use('/handmade/mobile', mobile_handmadeRouter);
 
 
-app.listen(3001, '0.0.0.0', () => {
-  console.log('Listening on http://0.0.0.0:3001');
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Listening on http://0.0.0.0:3000');
 });
 
